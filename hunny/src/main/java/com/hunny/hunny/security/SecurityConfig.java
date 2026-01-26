a30208ccb25b528560bcb9f93ac09a55fef90c7d
Suraj Chawhan <112802275+Suraj-chawhan@users.noreply.github.com>
2026-01-26 21:20:35 +0530

    Update SecurityConfig.java

--- hunny/src/main/java/com/hunny/hunny/security/SecurityConfig.java
+++ hunny/src/main/java/com/hunny/hunny/security/SecurityConfig.java
@@ -10,12 +10,11 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    // ✅ FIXED CORS CONFIG
+    
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        // IMPORTANT: use allowedOriginPatterns
         config.setAllowedOriginPatterns(List.of(
             "http://localhost:3000",
             "https://springboot-fullstack-eccomerce.vercel.app"
@@ -40,14 +39,14 @@ public class SecurityConfig {
             .cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                // VERY IMPORTANT
+                
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/auth/**").permitAll()
                 .requestMatchers("/api/admin/**").hasAuthority("ROLE_ADMIN")
                 .requestMatchers("/api/user/**").authenticated()
                 .anyRequest().permitAll()
             )
-            // JWT filter AFTER CORS & OPTIONS handling
+        
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
